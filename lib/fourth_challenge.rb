aa20af01933a313fd3d3abcc4ae5075880797d37
Kelsey Low <kklowlow@gmail.com>
2019-01-22 22:14:25 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,13 +6,13 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {
-          :name => "Benvolio"
-          :age => "17"
+          :name => "Benvolio",
+          :age => "17",
           :attitude => "worried"
         },
         {
-          :name => "Mercutio"
-          :age => "18"
+          :name => "Mercutio",
+          :age => "18",
           :attitude => "hot-headed"
         }
       ]
@@ -22,7 +22,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
       ]
    }
 }
